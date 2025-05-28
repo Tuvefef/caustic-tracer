709853fb2ea722cb4d6f8b342a9da28c204a54a5
Tuvefef <119376758+Tuvefef@users.noreply.github.com>
2025-05-28 15:28:43 -0600

    Create cubFUNC.hpp

---
+++ pragma/aditionalFUNC/cubFUNC.hpp
@@ -0,0 +1,8 @@
+#ifndef CUBIC_MACRO
+#define CUBIC_MACRO
+
+/* macro for future use since it is undefined but it does fulfill its function */
+
+#define macroPElR(i) ((i) * (i) * (2.0 - 2.0 * (i)))
+
+#endif
